933dfe59cb09e7e5b9fa3d9d38fc916b8bdd26a2
Max Harris <maxharris@gmail.com>
2021-09-08 19:41:28 -0400

    Functions to add and delete relationships

---
+++ relationships.py
@@ -0,0 +1,75 @@
+from typing import get_type_hints
+
+
+def add_relationship(class_dict, source, destination, rel_dict):
+    
+    found_source = False
+    found_dest = False
+
+    #Ensure both destination and source exist.
+
+    for key in class_dict:
+        if key == source:
+            found_source = True
+        if key == found_dest:
+            found_dest = True
+
+    if found_source == False and found_dest == False:
+        print("Invalid source and destination, both arguements must be existing classes.")
+        return
+
+    if found_source == False:
+        print("Invalid source, source must be an existing class.")
+        return
+
+    if found_dest == False:
+        print("Invalid destination, destination must be an existing class.")
+        return
+
+    #Ensure the relationship does not already exist
+    
+    if (source + "-" + destination) in rel_dict.keys():
+        print("Relationship already exists")
+        return
+
+    #Add an element of the relationship dictionary that contains the key:value pair
+    # "source-destination" : (obj<sourceName>, obj<destinationName>)
+    rel_dict[source + "-" + destination] = (class_dict[source], class_dict[destination])
+
+    #return the dictionary with the new relationship included
+    return rel_dict
+
+def delete_relationship (class_dict, source, destination, rel_dict):
+
+    found_source = False
+    found_dest = False
+
+    #Ensure both destination and source exist.
+
+    for key in class_dict:
+        if key == source:
+            found_source = True
+        if key == found_dest:
+            found_dest = True
+
+    if found_source == False and found_dest == False:
+        print("Invalid source and destination, both arguements must be existing classes.")
+        return
+
+    if found_source == False:
+        print("Invalid source, source must be an existing class.")
+        return
+
+    if found_dest == False:
+        print("Invalid destination, destination must be an existing class.")
+        return
+
+    #Ensure relationship actually exists
+    if (source + "-" + destination) not in rel_dict.keys():
+        print("Relationship does not exist")
+        return
+
+    del rel_dict[source + "-" + destination]
+
+    #Return the dictionary minus the deleted relationship
+    return rel_dict
