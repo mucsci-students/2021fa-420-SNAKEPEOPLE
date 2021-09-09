2322a6f3a2f6fcb3ed7f2c2f4bb75c958ca986b5
Max Harris <maxharris@gmail.com>
2021-09-08 22:42:07 -0400

    Functions to add and delete relationships

--- relationships.py
+++ relationships.py
@@ -1,8 +1,9 @@
 import uml_class
 
 uml_class.class_dict
+relationship_dict = dict()
 
-def add_relationship(source, destination, rel_dict):
+def add_relationship(source, destination):
     
     found_source = False
     found_dest = False
@@ -29,18 +30,18 @@ def add_relationship(source, destination, rel_dict):
 
     #Ensure the relationship does not already exist
     
-    if (source + "-" + destination) in rel_dict.keys():
+    if (source + "-" + destination) in relationship_dict.keys():
         print("Relationship already exists")
         return
 
     #Add an element of the relationship dictionary that contains the key:value pair
     # "source-destination" : (obj<sourceName>, obj<destinationName>)
-    rel_dict[source + "-" + destination] = (uml_class.class_dict[source], uml_class.class_dict[destination])
+    relationship_dict[source + "-" + destination] = (uml_class.class_dict[source], uml_class.class_dict[destination])
 
     #return the dictionary with the new relationship included
-    return rel_dict
+    return
 
-def delete_relationship (source, destination, rel_dict):
+def delete_relationship (source, destination):
 
     found_source = False
     found_dest = False
@@ -66,11 +67,11 @@ def delete_relationship (source, destination, rel_dict):
         return
 
     #Ensure relationship actually exists
-    if (source + "-" + destination) not in rel_dict.keys():
+    if (source + "-" + destination) not in relationship_dict.keys():
         print("Relationship does not exist")
         return
 
-    del rel_dict[source + "-" + destination]
+    del relationship_dict[source + "-" + destination]
 
     #Return the dictionary minus the deleted relationship
-    return rel_dict
+    return
