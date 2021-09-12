fecebc96917278722c61a7c8419fed3f97bd4bb7
Max Harris <maxharris@gmail.com>
2021-09-12 18:21:19 -0400

    Test file using unittest

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import unittest
+import io
+import sys
+import unittest.mock
 import uml_class
 import relationships
 
@@ -19,12 +22,52 @@ class test(unittest.TestCase):
         self.assertEqual(len(relationships.relationship_dict), 1)
 
         self.assertEqual(relationships.relationship_dict["class1-class2"], 
-                                (uml_class.class_dict["class1"], uml_class.class_dict["class2"]))
-        
-        self.assertEqual(relationships.relationship_dict["class1-class2"][1], 
-                                uml_class.class_dict["class2"])
+                    (uml_class.class_dict["class1"], uml_class.class_dict["class2"]))
 
-        
+        relationships.delete_relationship("class1", "class2")
+
+        self.assertEqual(len(relationships.relationship_dict), 0)
+
+        uml_class.delete_class("class1")
+
+        self.assertEqual(len(uml_class.class_dict), 1)
+
+        #Check that deleting a non-existant class will result in a printed error.
+        out = io.StringIO()
+        sys.stdout = out
+        uml_class.delete_class("class1")
+        sys.stdout = sys.__stdout__
+        self.assertEqual(out.getvalue(), "Error: class1 does not exist as the name of a class.\n")
+
+        #Check that adding a duplicate class will print an error message.
+        out = io.StringIO()
+        sys.stdout = out
+        uml_class.add_class("class2")
+        sys.stdout = sys.__stdout__
+        self.assertEqual(out.getvalue(), "Error: class2 is already the name of an existing class.\n")
+
+        #Check that adding a relationship without a source will print the appropriate message
+        out = io.StringIO()
+        sys.stdout = out
+        relationships.add_relationship("class1", "class2")
+        sys.stdout = sys.__stdout__
+        self.assertEqual(out.getvalue(), "Invalid source, source must be an existing class.\n")
+
+        uml_class.add_class("class1")
+        uml_class.delete_class("class2")
+
+        #Check that adding a relationship without a valid destination will fail
+        out = io.StringIO()
+        sys.stdout = out
+        relationships.add_relationship("class1", "class2")
+        sys.stdout = sys.__stdout__
+        self.assertEqual(out.getvalue(), "Invalid destination, destination must be an existing class.\n")
+
+        uml_class.delete_class("class1")
+
+        #Check that adding a relationship without a valid source and destination will result
+        #in an appropriate 
+
 
 if __name__ == "__main__":
     unittest.main()
