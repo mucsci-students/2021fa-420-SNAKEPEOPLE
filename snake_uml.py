ea73ff1fee6549981efae49b6860abe092bebf83
Will Killian <william.killian@millersville.edu>
2021-09-20 07:04:22 -0400

    UML v1.0.0 Release (#25)
    * demo
    
    * test
    
    * test
    
    * Undid demo changes to README.md
    
    * Added 'snake_uml.py' as an entry point to the program.
    
    * Added file contents.
    
    * Add UMLCLass and interfacing methods (#3)
    
    * Added representation for UMLClass.
    
    * Added .gitignore
    
    * Added methods for interfacing with UMLClass.
    
    * help/exit (#4)
    
    * Functions to add and delete relationships (#6)
    
    Co-authored-by: Max Harris <maxharris@gmail.com>
    
    * Updated the htlp_stuff.txt to print a little more clearly to the CLI
    
    * Updated the help_stuff.txt to print a little more clearly to the CLI
    
    * Updated the help_stuff.txt to print a little more clearly to the CLI
    
    * Updated the help_stuff.txt to print a little more clearly to the CLI
    
    * listing classes (#10)
    
    * Functions to add and delete relationships (#11)
    
    Co-authored-by: Max Harris <maxharris@gmail.com>
    
    * json update
    
    created temporary uml class to test interactions
    
    * Removed __pycache__
    
    * Attribute Add/Delete/Rename (#13)
    
    * Added functionality for adding, deleting, and renaming class attributes.
    
    * Refactored class functions, added better documentation.
    
    * Added attribute rename and delete functionality. Also refactored functions that took a UMLClass as a parameter to take strings instead.
    
    * delete_class update
    
    * main / help (#17)
    
    * Rel dev (#20)
    
    * Functions to add and delete relationships
    
    * Functions to A/D relationships and display them
    
    * Functions to A/D relationships and display them
    
    * Added overloaded function to delete all relationships associated with an input class
    
    * Added relationship class and cleaned up design
    
    * minor changes to error messages
    
    * ahhh
    
    * Added a function to update any relationship that may have an updated object contained within it
    
    * new failure case
    
    * new failure case
    
    Co-authored-by: Max Harris <maxharris@gmail.com>
    
    * main2 (#18)
    
    * main / help
    
    * main2
    
    Co-authored-by: Nicholas Bellman <58608676+nrbellman@users.noreply.github.com>
    
    * Delete UMLJson.json
    
    * Delete snake_json.py
    
    * Better json (#21)
    
    * better json implementation testing
    
    * Added save/load functionality and minor other bug-fixes.
    
    * Forgot to save a file.
    
    * Delete __pycache__ directory
    
    * Deleted test file.
    
    * .gitignore changes
    
    * .gitignore changes
    
    * .gitignore changes
    
    * .gitignore changes
    
    * Minor changes
    
    * UI (#23)
    
    * Refactored command parsing. Also cleaned up text output to be more consistent.
    
    * Deleted test file.
    
    * Added action confirmations for class/attribute commands.
    
    * Added confirmation messages for relationship functions.
    
    * Fixed that bug that Max harasses me about.
    
    * Minor changes
    
    * save/load/help (#24)
    
    Co-authored-by: Nicholas Bellman <58608676+nrbellman@users.noreply.github.com>
    
    * Integrated updated save/load into new ui changes.
    
    * .gitignore changes
    
    * Minor formatting changes in help_stuff.txt.
    
    * Minor formatting changes in help_stuff.txt.
    
    * Updated README with info to lauch program and added requirements.txt
    
    Co-authored-by: Nicholas Bellman <nbellman95@gmail.com>
    Co-authored-by: Nicholas Bellman <58608676+nrbellman@users.noreply.github.com>
    Co-authored-by: Curtis K <77931860+curtisjk717@users.noreply.github.com>
    Co-authored-by: Mharr1 <74013401+Mharr1@users.noreply.github.com>
    Co-authored-by: Max Harris <maxharris@gmail.com>
    Co-authored-by: Tanner Michael <tmichael@millersville.edu>
    Co-authored-by: Evan Hanzelman <ehanz1997@gmail.com>

---
+++ snake_uml.py
@@ -0,0 +1,236 @@
+# Project Name:  SNAKE PEOPLE UML Editor
+# File Name:     snake_uml.py
+
+# Imports:
+import json
+import jsonpickle
+import sys
+import os.path
+
+import uml_class
+import relationships
+
+def main(args : list) -> None:
+    '''
+    Processes an infinite loop waiting for a command from the user. Once input
+    is given, executes a command, if valid.
+
+    Parameters:\n
+    args : list -> A list of command-line arguments provided to the program.
+    '''
+    
+    print("=======================================\n" +
+          "|       Snake People UML Editor       |\n" +
+          "=======================================\n" +
+          "  Type 'help' for a list of commands.\n")
+    
+    while True:
+        
+        cmd = input(">> ").split()
+        
+        if len(cmd) == 0:
+            continue
+        
+        elif cmd[0] == 'exit' or cmd[0] == 'quit':
+            break
+        
+        elif cmd[0] == 'addclass':
+            if check_inputs(cmd, 2):
+                uml_class.add_class(cmd[1])
+            
+        elif cmd[0] == 'delclass':
+            if check_inputs(cmd, 2):
+                uml_class.delete_class(cmd[1])
+            
+        elif cmd[0] == 'renclass':
+            if check_inputs(cmd, 3):
+                uml_class.rename_class(cmd[1], cmd[2])
+                
+        elif cmd[0] == 'addrel':
+            if check_inputs(cmd, 3):
+                relationships.add_relationship(cmd[1], cmd[2])
+                
+        elif cmd[0] == 'delrel':
+            if check_inputs(cmd, 3):
+                relationships.delete_relationship(cmd[1], cmd[2])
+                
+        elif cmd[0] == 'addattr':
+            if check_inputs(cmd, 3):
+                uml_class.add_attribute(cmd[1], cmd[2])
+                
+        elif cmd[0] == 'delattr':
+            if check_inputs(cmd, 3):
+                uml_class.delete_attribute(cmd[1], cmd[2])
+                
+        elif cmd[0] == 'renattr':
+            if check_inputs(cmd, 4):
+                uml_class.rename_attribute(cmd[1], cmd[2], cmd[3])
+                
+        elif cmd[0] == 'listclass':
+            if check_inputs(cmd, 2):
+                if cmd[1] == 'all':
+                    list_all_classes()
+                else:
+                    list_a_class(cmd[1])
+                    
+        
+        elif cmd[0] == 'listrel':
+            relationships.list_relationships()
+        
+        elif cmd[0] == 'save':
+            if check_inputs(cmd, 2):
+                save_classes(cmd[1])
+        
+        elif cmd[0] == 'load':
+            if check_inputs(cmd, 2):
+                load_classes(cmd[1])
+        
+        elif cmd[0] == 'help':
+            help()
+        
+        else:
+            print("<Invalid Command Error>: " +
+                  f"'{cmd[0]}' is not a valid command.\n" +
+                  "Type 'help' for a list of valid commands.")
+    
+
+def check_inputs(cmd : list, num : int) -> bool:
+    """
+    Checks if the number of arguments given matches the number of expected
+    arguments for a given command.
+    
+    Parameters:\n
+    - cmd : list -> a list of commands/arguments parsed from user input.
+    - num : int -> the number of expected arguments
+    
+    return -> bool
+    """
+    
+    if len(cmd) != num:
+        # If the number of given arguments does not match the number of expected
+        # arguments, print an error and return False.
+        print("<Invalid Arguments Error>\n" +
+              f"{num - 1} arguments expected. {len(cmd) - 1} arguments " + 
+              "received.")
+        return False
+    else:
+        # Otherwise return True.
+        return True
+
+
+def help() -> None:
+    """
+    Reads help information from a separate text file and prints it to the
+    terminal window.
+    """
+    
+    # Reading the information from the help file.
+    help_file = open('help_stuff.txt')
+    lines = help_file.readlines()
+
+
+    # Printing everything from the help file.
+    for line in lines:
+        print(line)
+
+    help_file.close()
+
+def list_a_class(input : str) -> None:
+    """
+    Given a class name, if the class exists in the system, printz the name of 
+    the class and all the attributes it has.
+    
+    Parameters:\n
+    - input : str -> the name of the class to be listed.
+    """
+    
+    if input in uml_class.class_dict:
+        # Accesses a class from the class dictionary, and prints it to the 
+        # terminal.
+        print(uml_class.class_dict[input])
+    else:
+        # If the class does not exist in the class dictionary, prints an error.
+        print(f"<Illegal Argument Error>: {input} does not exist as a class.")
+
+def list_all_classes() -> None:
+    """
+    Prints a list of all classes in the class dictionary and their attributes. 
+    """
+
+    if len(uml_class.class_dict) == 0:
+        print("(none)")
+    else:
+        # Iterates through the class dictionary and prints each entry.
+        for key in uml_class.class_dict:
+            print(uml_class.class_dict[key])
+ 
+def save_classes(filename : str) -> None:
+    """
+    Saves the dictionary of classes to a .json file.
+    """
+    
+    # Checks if the class dictionary is empty and prints an error if so.
+    if len(uml_class.class_dict) == 0:
+        print("Error: There are no classes in the class dictionary.\n" +
+              "Save failed.")
+        return
+    
+    # Initializes a dictionary to store the information that will be converted
+    # to JSON format.
+    save_dict = dict()
+    # Iterates through the dictionary of classes, encoding each entry into JSON
+    for key in uml_class.class_dict:
+        # Encodes the UMLClass object stored as a value in the class dictionary
+        # into an easily decodable JSON format.
+        cls = jsonpickle.encode(uml_class.class_dict[key])
+        # Adds the JSON encoded object to 'save_dict' with the same key as in
+        # 'class_dict'.
+        save_dict.update({key : cls})
+        
+    # Opens a file stored in 'save_files/' where the JSON will be saved to.
+    with open(f"save_files/{filename}.json", "w") as savefile:
+        # Writes the contents of 'save_dict' as JSON to the file pointed to by 
+        # 'savefile'.
+        json.dump(save_dict, savefile)
+        
+def load_classes(filename : str) -> None:
+    """
+    Loads the content of a .json file to the class dictionary.
+    """
+    
+    # Prints a warning that the current class dictionary will be overwritten 
+    # upon load.
+    print("Warning: Loading will overwrite any unsaved changes.")
+    # Gets confirmation from user (default yes).
+    cont = input("Continue loading? ([y]/n): ")
+    
+    # Checks if the user confirmed the load or not.
+    if cont == "" or cont.lower() == "y":
+        # If the user confirms the load, clears the class dictionary.
+        uml_class.class_dict.clear()
+
+        # Checking that the file the user is trying to load is one that exists.
+        if os.path.exists(f"save_files/{filename}.json"):
+            # Intializes a new dictionary to store the loaded JSON data.
+            load_dict = dict()
+            # Opens 'classes.json' for reading, pointed to as 'loadfile'.
+            with open(f"save_files/{filename}.json") as loadfile:
+                # Loads the raw JSON into 'load_dict'
+                load_dict = json.load(loadfile)
+            
+            # Iterates through the keys and values of 'load_dict'. 
+            for key, value in load_dict.items():
+                # Decodes the UMLClass objects and adds them as values to the class
+                # dictionary.
+                uml_class.class_dict.update({key : jsonpickle.decode(value)})
+        # If the file name doesn't exist, stops and tells the user.
+        else:
+            print("Load cancelled, please input the name of an existing file.")
+
+    else:
+        # If load is not confirmed, print a cancellation message.
+        print("Load cancelled.")
+ 
+# Entry Point
+if __name__ == '__main__':
+    main(sys.argv)
