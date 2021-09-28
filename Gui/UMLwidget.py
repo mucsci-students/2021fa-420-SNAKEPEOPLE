b83a4e9aefb47fa9fc4d04acf0da8784210c2aeb
Max Harris <maxharris@gmail.com>
2021-09-27 21:27:23 -0400

    Fixed counter variable so added classes do not overlap

--- Gui/UMLwidget.py
+++ Gui/UMLwidget.py
@@ -1,25 +1,26 @@
 import tkinter as tk
+from tkinter.constants import CURRENT
 import DnD
 
 label_list = []
-currentx = 0
-currenty = 0
 
 class UMLwidget():
 
+    currentx = 0
+    currenty = 0
     #create a new label with the name provided and pack it into the frame
     def __init__(self, name : str, frame):
         self.name = name
         self.label = tk.Label(frame, bg="#10f", width="10", height = "5", text = name)
-        if(currentx == currenty):
-            ++currentx
-        elif(currentx > currenty):
-            ++currenty  
-        self.label.grid(row=currentx, column=currenty)      
+        self.label.grid(row=UMLwidget.currentx, column=UMLwidget.currenty)      
 
 #Add the UMLwidget object to the frame and to the list
 #Then call upon drag and drop (DnD) to allow it to move within the frame
 def add_widget(name : str, frame):
+    if(UMLwidget.currentx == UMLwidget.currenty):
+        UMLwidget.currentx = UMLwidget.currentx + 1
+    elif(UMLwidget.currentx > UMLwidget.currenty):
+        UMLwidget.currenty = UMLwidget.currenty + 1
     new_widget = UMLwidget(name, frame)
     label_list.append(new_widget)
     DnD.add_drag(new_widget.label)
