6dc367595b172fa56e2aac9ab86f468139e413ac
Nicholas Bellman <nbellman95@gmail.com>
2021-09-29 12:53:08 -0400

    Documented JSON.py

--- JSON.py
+++ JSON.py
@@ -9,33 +9,76 @@ class ComplexEncoder(json.JSONEncoder):
     A complex json encoder for encoding nested objects.
     """
     
-    def default(self, obj: Union[UMLClass or UMLRelationship]) -> dict:
+    def default(self, 
+                obj: Union[UMLClass or UMLRelationship]) -> dict:
         if hasattr(obj, 'toJson'):
             return obj.toJson()
         else:
             return json.JSONEncoder.default(self, obj)
         
-def encode(classes : list, relationships : list) -> str:
+def encode(classes : list[dict], 
+           relationships : list[dict]) -> str:
+    """
+    Encodes lists of classes and relationships into JSON.
+    
+    Parameters:
+    - classes : list[dict] -> all classes to be encoded into JSON.
+    - relationships : list[dict] -> all relationships to be encoded
+    into JSON.
+    
+    return : str -> JSON representation of a dictionary of the parameter lists. 
+    """
+    
+    # Dictionary containing lists of classes and relationships.
     objects = {"classes" : classes,
                "relationships" : relationships}
     
+    # Returns the result of the json.dumps() method, which is a string.
     return json.dumps(objects, indent= 4, cls= ComplexEncoder)
 
 
-def decode_classes(classes : dict) -> dict:
+def decode_classes(classes : list) -> dict:
+    """
+    Decodes JSON into python objects representing a UMLClass and it's fields and
+    methods, including method paramters.
+    
+    Parameters:
+    - classes : list -> the list of classes derived from the dictionary returned
+    by json.loads().
+    
+    returns : dict -> a dictionary of UMLClasses where the key is the name of 
+    the UMLClass. The internally stored class dictionary should point to this
+    value.
+    """
+    
+    # Function's nternal class dictionary.
     cls_dict = {}
     
+    # Loops through the list of classes.
     for cls in classes:
+        
+        # Lists of UMLAttribute Field and Method objects that exist in the 
+        # class.
         fields = []
         methods = []
     
+        # Loops through the list of JSON representaions of fields and creating
+        # UMLAttribute Field objects.
         for field in cls['fields']:
             fields.append(Field(**field))
         
+        # Loops through the list of JSON representations of methods and creating
+        # UMLAttribute Method objects and their parameters (if they exist). Then
+        # appends the Method objects to the list 'methods'.
         for method in cls['methods']:
+            
+            # List of UMLAttribute Parameter objects
             params = []
             m_obj = Method(**method)
             
+            # Loops through the list of JSON representations of parameters
+            # creating UMLAttribute Parameter objects and appending them to
+            # 'params'.
             for param in method['params']:
                 p_obj = Parameter(**param)
                 params.append(p_obj)
@@ -43,18 +86,37 @@ def decode_classes(classes : dict) -> dict:
             m_obj.params = params    
             methods.append(m_obj)
             
+        # Constructs a new UMLClass object with paramters derived from 'cls'
         obj : UMLClass = UMLClass(**cls)
+        # Assigns the list of fields and methods to the UMLClass objects fields
+        # of the same name.
         obj.fields = fields
         obj.methods = methods
         
+        # Adds the object to the internal class dictionary.
         cls_dict.update({obj.name : obj})
         
     return cls_dict
 
 
-def decode_relationships(relationships : dict) -> list:
+def decode_relationships(relationships : list) -> list:
+    """
+    Decodes JSON into python objects representing a UMLRelationship.
+    
+    Parameters:
+    - relationships : list -> the list of relationships derived from the 
+    dictionary returned by json.loads().
+    
+    return : list -> a list containing UMLRelationship objects. The internal
+    relationship list should point to this value.
+    """
+    
+    # Function's internal relationship list.
     rel_list = []
     
+    # Loops through the list of JSON representations of relationships, creating
+    # UMLRelationship objects and appending those objects to the function's
+    # internal relationship list.
     for rel in relationships:
         obj : UMLRelationship = UMLRelationship(**rel)
         rel_list.append(obj)
@@ -63,8 +125,21 @@ def decode_relationships(relationships : dict) -> list:
 
 
 def decode(json_str : str) -> tuple[dict,list]:
+    """
+    Decodes JSON into the respective UML components objects.
+    
+    Parameters:
+    - json_str : str -> the encoded json string of UML component objects.
+    
+    return : tuple[dict,list] -> a tuple of the class dictionary returned by 
+    'decode_classes' and the relationship list returned by 
+    'decode_relationships'.
+    """
+    
+
     json_dict = json.loads(json_str)
     
+    # Pulls the lists of classes and relationships from json_dict.
     classes = json_dict['classes']
     relationships = json_dict['relationships']
     
@@ -99,17 +174,14 @@ if __name__ == "__main__":
     
     xmp = encode(cls_list, rel_list)
     print(xmp)
+    
     input()
     json_dict = json.loads(xmp)
     class_json = json_dict['classes']
     rel_json = json_dict['relationships']
     
-    input()
     old_cd = class_dict
     class_dict, rel_list = decode(xmp)
-    print(class_dict)
+    print (class_dict)
     print (rel_list)
-    
-    
-    
     
--- uml_components/interfaces/rel_interface.py
+++ uml_components/interfaces/rel_interface.py
@@ -1,6 +1,67 @@
 from uml_components.UMLClass import class_dict
 from uml_components.UMLRelationship import UMLRelationship, relationship_list
 
+def check_class(cls : str) -> bool:
+    """
+    Checks if the given class exists in the class dictionary.
+    
+    Parameters:
+    - cls : str -> the name of the class to be checked for.
+    
+    Returns:
+    - found : bool -> True if cls exists, False otherwise.
+    """
+    
+    found : bool = False
+    
+    # Loops through the class dictionary and sets found to True if 'cls' is in
+    # the class dictionary.
+    for key in class_dict:
+        if cls == key:
+            found = True
+            
+    return found
+
+
+def check_type(rel_type : str) -> bool:
+
+    """
+    Checks to ensure that the relationship type is one of Aggregation, 
+    Composition, Inheritance, or Realization.
+
+    - rel_type : str -> the type to be checked.
+    """
+
+    # A set of valid relationship types.
+    relationship_types = {"aggregation", 
+                          "composition", 
+                          "inheritance", 
+                          "realization"}
+    
+    if rel_type.lower() in relationship_types:
+        return True
+    else:
+        return False
+
+def find_rel(source : str, dest : str) -> tuple[bool, int]:
+    index : int = 0
+    
+    if check_class(source) and check_class(dest): 
+        relationship : UMLRelationship
+        for relationship in relationship_list:
+            # If source and destination are both valid, iterates through the
+            # relationship list until it finds a matching relationship.
+            if (source == relationship.source and 
+                dest == relationship.destination):
+                # If the source and destination match the source and destination
+                # of the current iteration, returns True.
+                return (True, index)
+            else:
+                # Increments the index by 1 and checks again.
+                index += 1
+                
+    return (False, index)
+
 def add_relationship(source : str, destination : str, rel_type : str) -> None:
 
     """
@@ -63,36 +124,19 @@ def delete_relationship(source : str, dest : str) -> None:
     if not check_class(dest):
         print("<Relationship Delete Error>: " +
               f"{dest} does not exist as the name of a class.")
-        
-    if check_class(source) and check_class(dest):
-        index : int = 0
-        found : bool = False
-        
-        relationship : UMLRelationship
-        for relationship in relationship_list:
-            # If source and destination are both valid, iterates through the
-            # relationship list until it finds a matching relationship.
-            if (source == relationship.source and 
-                dest == relationship.destination):
-                # If the source and destination match the source and destination
-                # of the current iteration, set found to True and break the 
-                # loop.    
-                found = True
-                break
-            else:
-                # Increments the index by 1 and checks again.
-                index += 1
-        
-        # Checks whether a valid relationship was found.
-        if found:
-            # If matching relationship found, deletes the relationship from the
-            # list.
-            rel = relationship_list.pop(index)
-            print(f"<Deleted Relationship>: {source} - {dest} ({rel.type})")
-        else:
-            # Otherwise, prints an error.
-            print("<Relationship Delete Error>: " +
-                  f"Relationship {source} - {dest} does not exist.")
+            
+    
+    # Checks whether a valid relationship was found.
+    found, index = find_rel(source, dest)
+    if found:
+        # If matching relationship found, deletes the relationship from the
+        # list.
+        rel = relationship_list.pop(index)
+        print(f"<Deleted Relationship>: {source} - {dest} ({rel.type})")
+    else:
+        # Otherwise, prints an error.
+        print("<Relationship Delete Error>: " +
+                f"Relationship {source} - {dest} does not exist.")
             
 
 def rel_cleanup(cls : str) -> None:
@@ -130,9 +174,8 @@ def list_relationships() -> None:
         for relationship in relationship_list:
             print(relationship)
 
-##########################################################################################
 
-def Change_reltype(source : str, dest : str, new_type : str):
+def change_type(source : str, dest : str, new_type : str):
 
     #Checks to ensure that the new relationship type is one of Aggregation,
     #Composition, Inheritance, or Realization and then replaces the old 
@@ -143,68 +186,17 @@ def Change_reltype(source : str, dest : str, new_type : str):
     #destination- String that is set as the name of a UMLClass object
     #new_type- String of the new relationship type
 
-    found = False
-    rel_index = 0
-    while rel_index < len(relationship_list):
-        if ((relationship_list[rel_index].sourceName == source) 
-              and (relationship_list[rel_index].destName == dest)):
-            found = True
-            break
-        ++rel_index
 
     #Check to make sure the relationship exists
+    found, index = find_rel(source, dest)
     if found == False:
-        print(f"<Relationship Type Error>: "+
-            f"Relationship {source}-{dest} is an invalid relationship.")
+        print("<Relationship Type Error>: " +
+              f"{source}-{dest} is not a valid relationship.")
     #Check to make sure the new relationship type exists
     elif check_type(new_type) == False:
-        print(f"<Relationship Type Error>: "+
-                f"Relationship type {new_type} is an invalid type.")
+        print("<Relationship Type Error>: " +
+              f"{new_type} is not a valid relationship type.")
     else:
         #Update the relationship type
-        relationship_list[rel_index].changeType(new_type)
-
-##########################################################################################
-
-def check_class(cls : str) -> bool:
-    """
-    Checks if the given class exists in the class dictionary.
-    
-    Parameters:
-    - cls : str -> the name of the class to be checked for.
-    
-    Returns:
-    - found : bool -> True if cls exists, False otherwise.
-    """
-    
-    found : bool = False
-    
-    # Loops through the class dictionary and sets found to True if 'cls' is in
-    # the class dictionary.
-    for key in class_dict:
-        if cls == key:
-            found = True
-            
-    return found
-
-
-def check_type(rel_type : str) -> bool:
-
-    """
-    Checks to ensure that the relationship type is one of Aggregation, 
-    Composition, Inheritance, or Realization.
-
-    - rel_type : str -> the type to be checked.
-    """
-
-    # A set of valid relationship types.
-    relationship_types = {"aggregation", 
-                          "composition", 
-                          "inheritance", 
-                          "realization"}
-    
-    if rel_type.lower() in relationship_types:
-        return True
-    else:
-        return False
-
+        relationship : UMLRelationship = relationship_list[index]
+        relationship.change_type(new_type)
