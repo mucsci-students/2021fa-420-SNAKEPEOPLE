c1859f247dfd5f490344d07976a0213db1874da2
Max Harris <maxharris@gmail.com>
2021-10-21 19:56:24 -0400

    added better canvas population method

--- Gui/ViewChange.py
+++ Gui/ViewChange.py
@@ -7,6 +7,9 @@ def del_item(name):
 def bring_front(item):
     UMLBox.test_canvas.tag_raise(item)
 
+def push_back(item):
+    UMLBox.test_canvas.tag_lower(item)
+
 def set_rec(item, x1, y1, x2, y2):
     UMLBox.test_canvas.coords(item, x1, y1, x2, y2)
 
@@ -21,3 +24,12 @@ def item_config(item, text, justify, state):
         UMLBox.test_canvas.itemconfigure(item, text = text)
     else:
         UMLBox.test_canvas.itemconfigure(item, text = text, justify = justify, state = state)
+
+def bring_all_front(obj):
+    bring_front(obj.rec)
+    bring_front(obj.methodlabel)
+    bring_front(obj.methodtext)
+    bring_front(obj.fieldlabel)
+    bring_front(obj.fieldtext)
+    bring_front(obj.label)
+        
