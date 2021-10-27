db752d2cb6df1de66e89c60ee3b1350b98da523e
Evan Hanzelman <ehanz1997@gmail.com>
2021-10-27 09:43:37 -0400

    modified more tests

--- tests/py_test/uml_attribute_py_test.py
+++ tests/py_test/uml_attribute_py_test.py
@@ -7,37 +7,68 @@ import snake_uml
 from uml_components.interfaces import class_interface, attr_interface, rel_interface
 
 def test_add_field () :
-    attr_interface.add_field (class_name, field_name, field_type)
+    class_interface.add_class ("class1")
+    attr_interface.add_field ("class1", "attr1", "type1")
+    #assert attr_interface.class_dict["class1"].__getattribute__ == "attr1"
+    assert attr_interface.find_field ("class1", "attr1") == True
 
 def test_add_method () :
-    attr_interface.add_method (class_name, method_name, method_type)
+    class_interface.add_class ("class2")
+    attr_interface.add_method ("class2", "method2", "type2")
+    assert attr_interface.find_method ("class2", "method2") == True
 
 def test_add_param () :
-    attr_interface.add_param (class_name, method_name, param_name, param_type)
+    class_interface.add_class ("class3")
+    attr_interface.add_param ("class3", "method3", "param3", "type3")
+    attr_interface.find_param ("method3", "param3") == True
 
 def test_rename_field () :
-    attr_interface.rename_field (class_name, field_name, new_name)
+    class_interface.add_class ("class4")
+    attr_interface.add_field ("class4", "field4", "type4")
+    attr_interface.rename_field ("class4", "field4", "newfield")
+    assert attr_interface.find_field ("class4", "newfield") == True
 
 def test_rename_method () :
-    attr_interface.rename_method (class_name, method_name, new_name)
+    class_interface.add_class ("class5")
+    attr_interface.add_method ("class5", "method5", "type5")
+    attr_interface.rename_method ("class5", "method5", "newmethod")
+    attr_interface.find_method ("class5", "newmethod") == True
 
 def test_rename_param () :
-    attr_interface.rename_param (class_name, method_name, param_name, new_name)
+    class_interface.add_class ("class6")
+    attr_interface.add_param ("class6", "method6", "param6", "type6")
+    attr_interface.rename_param ("class6", "method6", "param6", "newparam")
+    assert attr_interface.find_param ("method6", "newparam") == True
 
 def test_delete_field () :
-    attr_interface.delete_field (class_name, field_name)
+    class_interface.add_class ("class7")
+    attr_interface.add_field ("class7", "field7", "type7")
+    attr_interface.delete_field ("class7", "field7")
+    assert attr_interface.find_field ("class7", "field7") == False
 
 def test_delete_method () :
-    attr_interface.delete_method (class_name, method_name)
+    class_interface.add_class ("class8")
+    attr_interface.add_method ("class8", "method8", "type8")
+    attr_interface.delete_method ("class8", "method8")
+    assert attr_interface.find_method ("class8", "method8") == False
 
 def test_delete_param () :
-    attr_interface.delete_param (class_name, method_name, param_name)
+    class_interface.add_class ("class9")
+    attr_interface.add_param ("class9", "method9", "param9", "type9")
+    attr_interface.delete_param ("class9", "method9", "param9")
+    attr_interface.find_param ("method9", "param9") == False
 
 def test_find_field () :
-    attr_interface.find_field (uml, field_name)
+    class_interface.add_class ("class10")
+    attr_interface.add_field ("class10", "field10")
+    assert attr_interface.find_field ("class10", "field10") == True
 
 def test_find_method () :
-    attr_interface.find_method (uml, method_name)
+    class_interface.add_class ("class11")
+    attr_interface.add_method ("class11", "method11", "type11")
+    attr_interface.find_method ("class11", "method11") == True
 
 def test_find_param () :
-    attr_interface.find_param (method, param_name)
+    class_interface.add_class ("class12")
+    attr_interface.add_param ("class12", "method12", "param12", "type12")
+    attr_interface.find_param ("method12", "param12")
--- tests/py_test/uml_class_py_test.py
+++ tests/py_test/uml_class_py_test.py
@@ -14,8 +14,8 @@ def test_empty_uml () :
 def test_two_uml_class () :
     class_interface.add_class ("class1")
     class_interface.add_class ("class2")
-    assert class_interface.__name__ == "class1"
-    assert class_interface.__name__ == "class2"
+    assert class_interface.class_dict["class1"].__name__ == "class1"
+    assert class_interface.class_dict["class2"].__name__ == "class2"
 
 # check input for improper argument lengths
 def test_input_length_uml_class () :
@@ -24,7 +24,7 @@ def test_input_length_uml_class () :
     snake_uml.main (sys.argv)
     sys.stdout = sys.__stdout__
     assert "Error: arguments expected, 0 arguments received" in out.getvalue () == True
-    assert len (class_interface) == 0
+    assert len (class_interface.class_dict) == 0
     del out
 
 # check invalid input of proper length
@@ -36,7 +36,7 @@ def test_input_invalid_uml_class () :
     assert "Error: invalid command." in out.getvalue () == True
     assert len (class_interface.class_dict) == 0
     del out
-    #class_interface = {}
+    class_interface.class_dict = {}
 
 # check adding a duplicate uml class
 def test_duplicate_uml_class () :
@@ -46,8 +46,10 @@ def test_duplicate_uml_class () :
     sys.stdout = out
     snake_uml.main (sys.argv)
     sys.stdout = sys.__stdout__
+    assert class_interface.class_dict["class1"].__name__== "class1"
     assert "Class named 'class1' already exists.\n" in out.getvalue () == True
     del out
+    class_interface.class_dict = {}
 
 # check deleting a uml class
 def test_delete_uml_class () :
@@ -57,21 +59,20 @@ def test_delete_uml_class () :
     out = io.StringIO ()
     sys.stdout = out
     sys.stdout = sys.__stdout__
-    assert class_interface == {}
-    assert len (class_interface) == 0
+    assert class_interface.class_dict == {}
+    assert len (class_interface.class_dict) == 0
 
 # check deleting a non-existant uml class
 def test_delete_ne_uml_class () :
-    uml = UMLClass ()
-    uml.class_dict.delete_class ("class1")
+    class_interface.delete_class ("class1")
     out = io.StringIO ()
     sys.stdout = out
     snake_uml.main (sys.argv)
     sys.stdout = sys.__stdout__
     assert "Class named 'class 1' does not exist." in out.getvalue () == True
-    assert len (uml.class_dict) == 0
+    assert len (class_interface.class_dict) == 0
     del out
-    uml.class_dict = {}
+    class_interface.class_dict = {}
 
 # check renaming a uml class
 def test_rename_uml_class () :
@@ -81,13 +82,13 @@ def test_rename_uml_class () :
     sys.stdout = out
     snake_uml.main (sys.argv)
     sys.stdout = sys.__stdout__
-    assert class_interface.__name__ == "class2"
-    assert len (class_interface) == 1
+    assert class_interface.class_dict["class2"] == "class2"
+    assert len (class_interface.class_dict) == 1
     del out
+    class_interface.class_dict = {}
 
 # check renaming a nonexistant uml class
 def test_rename_ne_uml_class () :
-    uml = UMLClass ()
     class_interface.rename_class ("class1", "class2")
     out = io.StringIO ()
     sys.stdout = out
@@ -95,6 +96,7 @@ def test_rename_ne_uml_class () :
     sys.stdout = sys.__stdout__
     assert "Error: class1 does not exist as the name of a class." in out.getvalue () == True
     del out
+    class_interface.class_dict = {}
 
 # check renaming an already existing uml class
 def test_rename_ae_uml_class () :
@@ -106,5 +108,6 @@ def test_rename_ae_uml_class () :
     snake_uml.main (sys.argv)
     sys.stdout = sys.__stdout__
     assert "Error: Class name 'class2' already exists." in out.getvalue () == True
-    assert len (class_interface) == 2
+    assert len (class_interface.class_dict) == 2
     del out
+    class_interface.class_dict = {}
