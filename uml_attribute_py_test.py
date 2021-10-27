d0ee90f7da7d4c52c60f53a886c2f856a799ef1e
Evan Hanzelman <ehanz1997@gmail.com>
2021-10-27 17:41:20 -0400

    more updates...

--- uml_attribute_py_test.py
+++ uml_attribute_py_test.py
@@ -4,9 +4,8 @@ from uml_components.interfaces import class_interface, attr_interface, rel_inter
 
 def test_add_field () :
     class_interface.add_class ("class1")
-    attr_interface.add_field ("class1", "attr1", "type1")
+    assert attr_interface.add_field ("class1", "attr1", "type1")
     #assert attr_interface.class_dict["class1"].__getattribute__ == "attr1"
-    assert attr_interface.find_field ("class1", "attr1") == (True, "attr1")
 
 def test_add_method () :
     class_interface.add_class ("class2")
@@ -15,56 +14,46 @@ def test_add_method () :
 
 def test_add_param () :
     class_interface.add_class ("class3")
-    attr_interface.add_param ("class3", "method3", "param3", "type3")
-    assert attr_interface.find_param ("method3", "param3") == (True, "param3")
+    assert attr_interface.add_param ("class3", "method3", "param3", "type3")
 
 def test_rename_field () :
     class_interface.add_class ("class4")
     attr_interface.add_field ("class4", "field4", "type4")
-    attr_interface.rename_field ("class4", "field4", "newfield")
-    assert attr_interface.find_field ("class4", "newfield") == (True, "newfield")
+    assert attr_interface.rename_field ("class4", "field4", "newfield")
 
 def test_rename_method () :
     class_interface.add_class ("class5")
     attr_interface.add_method ("class5", "method5", "type5")
-    attr_interface.rename_method ("class5", "method5", "newmethod")
-    method = UMLMethod ("", "")
-    assert attr_interface.find_method ("class5", "newmethod") == (True, method)
+    assert attr_interface.rename_method ("class5", "method5", "newmethod")
 
 def test_rename_param () :
     class_interface.add_class ("class6")
     attr_interface.add_param ("class6", "method6", "param6", "type6")
-    attr_interface.rename_param ("class6", "method6", "param6", "newparam")
-    assert attr_interface.find_param ("method6", "newparam") == (True, "newparam")
+    assert attr_interface.rename_param ("class6", "method6", "param6", "newparam")
 
 def test_delete_field () :
     class_interface.add_class ("class7")
     attr_interface.add_field ("class7", "field7", "type7")
-    attr_interface.delete_field ("class7", "field7")
+    assert attr_interface.delete_field ("class7", "field7")
 
 def test_delete_method () :
     class_interface.add_class ("class8")
     attr_interface.add_method ("class8", "method8", "type8")
-    attr_interface.delete_method ("class8", "method8")
-    assert attr_interface.find_method ("class8", "method8") == False
+    assert attr_interface.delete_method ("class8", "method8")
 
 def test_delete_param () :
     class_interface.add_class ("class9")
     attr_interface.add_param ("class9", "method9", "param9", "type9")
-    attr_interface.delete_param ("class9", "method9", "param9")
-    assert attr_interface.find_param ("method9", "param9") == False
+    assert attr_interface.delete_param ("class9", "method9", "param9")
 
 def test_find_field () :
     class_interface.add_class ("class10")
-    attr_interface.add_field ("class10", "field10")
-    assert attr_interface.find_field ("class10", "field10") == True
+    assert attr_interface.add_field ("class10", "field10", "type10")
 
 def test_find_method () :
     class_interface.add_class ("class11")
-    attr_interface.add_method ("class11", "method11", "type11")
-    assert attr_interface.find_method ("class11", "method11") == True
+    assert attr_interface.add_method ("class11", "method11", "type11")
 
 def test_find_param () :
     class_interface.add_class ("class12")
-    attr_interface.add_param ("class12", "method12", "param12", "type12")
-    assert attr_interface.find_param ("method12", "param12")
+    assert attr_interface.add_param ("class12", "method12", "param12", "type12")
