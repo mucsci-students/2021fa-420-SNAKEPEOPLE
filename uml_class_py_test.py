482b798c9ea6cb99184a03dd4733b68234e41c17
Nicholas Bellman <58608676+nrbellman@users.noreply.github.com>
2021-10-31 18:30:56 -0400

    Pytest (#53)
    * Create py_test.py
    
    * Update py_test.py
    
    * moved the file
    
    * moved to a specific py_test folder
    
    * Update py_test.py
    
    * Create uml_class_py_test.py
    
    * working on the tests
    
    * Added UMLClass test cases
    
    * updated UMLRelationship tests
    
    * added tests for UMLAttributes
    
    * changed and updated all tests
    
    * added init__py
    
    * tests
    
    * Update uml_attribute_py_test.py
    
    * modified more tests
    
    * adjusted more tests
    
    * updated tests
    
    * test remakes begin
    
    * changed tests more
    
    * rebuilt class tests
    
    * more updates...
    
    * more test changes
    
    * testsssssss
    
    * Update uml_attribute_py_test.py
    
    * Update uml_relationship_py_test.py
    
    * Update uml_attribute_py_test.py
    
    * Added return types to interface functions for easier testing.
    
    * changed indexes
    
    * Added ci.yml for GitHub Actions
    
    * hissssssssssss
    
    * Updated ci.yml for GitHub Actions
    
    * Update uml_attribute_py_test.py
    
    Co-authored-by: Evan Hanzelman <ehanz1997@gmail.com>

--- uml_class_py_test.py
+++ uml_class_py_test.py
@@ -3,24 +3,32 @@ from uml_components.interfaces import class_interface
 
 def test_add_class () :
     assert class_interface.add_class ("class09")
+    assert isinstance (class_interface.add_class ("class2"), tuple)
 
 def test_rename_class () :
     assert class_interface.rename_class ("class09", "class10")
+    assert isinstance (class_interface.rename_class ("class10", "class1"), tuple)
 
 def test_delete_class () :
     assert class_interface.delete_class ("class09")
+    assert class_interface.add_class ("class69")
+    assert isinstance (class_interface.delete_class ("class69"), tuple)
 
 def test_add_empty_class () :
     assert class_interface.add_class ("")
+    assert isinstance (class_interface.add_class (""), tuple)
 
 def test_add_empty_rename () :
     assert class_interface.add_class ("class10")
     assert class_interface.rename_class ("class10", "")
+    assert isinstance (class_interface.rename_class ("", "class20"), tuple)
 
 def test_delete_empty_class () :
     assert class_interface.add_class ("class11")
     assert class_interface.delete_class (" ")
+    assert class_interface.add_class ("emptyclass")
+    assert isinstance (class_interface.delete_class ("emptyclass"), tuple)
 
 def test_delete_ne_class () :
     assert class_interface.delete_class ("class11")
-    assert class_interface.delete_class ("class15")
+    assert isinstance (class_interface.delete_class ("class15"), tuple)
--- uml_relationship_py_test.py
+++ uml_relationship_py_test.py
@@ -4,20 +4,35 @@ from uml_components.interfaces import rel_interface, class_interface
 import pytest
 
 # check a relationship can be added
-# def test_add_rel () :
-#     index = 0
-#     class_interface.add_class ("source1")
-#     class_interface.add_class ("destination1")
-#     rel_interface.add_relationship ("source1", "destination1", "compostition")
-#     assert rel_interface.find_rel ("source1", "destination1") == (True, index)
+def test_add_rel () :
+    index = 0
+    class_interface.add_class ("source1")
+    class_interface.add_class ("destination1")
+    rel_interface.add_relationship ("source1", "destination1", "compostition")
+    assert rel_interface.find_rel ("source1", "destination1") == (False, index)
+
+def test_add_rel_two () :
+    index = 0
+    class_interface.add_class ("sourceT")
+    class_interface.add_class ("destT")
+    rel_interface.add_relationship ("sourceT", "destination1", "composition")
+    assert rel_interface.find_rel ("sourceT", "dest") == (False, index)
 
 def test_del_rel () :
-    index = 0
+    index = 1
     class_interface.add_class ("source2")
     class_interface.add_class ("destination2")
     rel_interface.add_relationship ("source2", "destination2", "composition")
     rel_interface.delete_relationship ("source2", "destination2")
     assert rel_interface.find_rel ("source2", "destination2") == (False, index)
+
+def test_rel_del_two () :
+    index = 1
+    class_interface.add_class ("Rocky")
+    class_interface.add_class ("Balboa")
+    rel_interface.add_relationship ("Rocky", "Balboa", "aggregation")
+    rel_interface.delete_relationship ("Rocky", "Adrian")
+    assert rel_interface.find_rel ("Rocky", "Balboa") == (True, index)
 
 def test_check_type () :
     class_interface.add_class ("source3")
@@ -27,14 +42,18 @@ def test_check_type () :
     assert rel_interface.check_type ("aggregation") == True
 
 def test_find_rel () :
-    index = 1
+    index = 3
     class_interface.add_class ("source4")
     class_interface.add_class ("destination4")
     rel_interface.add_relationship ("source4", "destination4", "aggregation")
     assert rel_interface.find_rel ("source4", "destination4") == (True, index)
 
+def test_find_rel_two () :
+    index = 0
+    assert rel_interface.find_rel ("source4", "DESTFAIL") == (False, index)
+
 def test_rel_cleanup () :
-    index = 2
+    index = 4
     class_interface.add_class ("source5")
     class_interface.add_class ("destination5")
     rel_interface.add_relationship ("source5", "destination5", "aggregation")
@@ -46,6 +65,16 @@ def test_change_type () :
     rel_interface.change_type ("source6", "destination6", "composition")
     assert rel_interface.check_type ("composition") == True
 
+def test_change_type_two () :
+    class_interface.add_class ("STypeFail")
+    class_interface.add_class ("DTypeFail")
+    rel_interface.add_relationship ("STypeFail", "DTypeFail", "aggregation")
+    assert rel_interface.check_type ("TYPEFAIL") == False
+
 def test_check_class () :
     class_interface.add_class ("classChk")
     assert rel_interface.check_class ("classChk") == True
+
+def test_check_class_two () :
+    class_interface.add_class ("classFail")
+    assert rel_interface.check_class ("FAIL") == False
