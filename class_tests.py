985144beae2580f06ec63052b7564a6f80fc0913
Evan Hanzelman <ehanz1997@gmail.com>
2021-11-22 14:06:39 -0500

    Create class_tests.py

---
+++ class_tests.py
@@ -0,0 +1,25 @@
+import pytest
+from uml_components import UMLAttributes
+from uml_components import UMLClass
+from uml_components import UMLRelationship
+
+#TODO write tests for UMLAttributes
+
+
+
+
+
+
+#TODO write tests for UMLClass
+
+
+
+
+
+
+#TODO write tests for UMLRelationship
+
+
+
+
+
